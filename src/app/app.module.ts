cbf7fe427bcb892fdcacec2dc095f8d6e9b6e4d5
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-30 18:36:02 +0530

    URL notfound route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,18 @@ import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 //DEFINE ROUTE
 const routes: Routes = [
   // { path: '', component: HomeComponent },
   { path: '', redirectTo: 'Home', pathMatch: 'full' }, // we can use line 16 or 17 if empty route to redirect to component
   { path: 'Home', component: HomeComponent },
-  { path: 'About', component: AboutComponent },
+
   { path: 'Contact', component: ContactComponent },
   { path: 'Courses', component: CoursesComponent },
+  { path: '**', component: NotFoundComponent }, //it should be defined at the end
+  { path: 'About', component: AboutComponent }, // this will also falls under ** so directed to notfound component
 ];
 
 @NgModule({
@@ -30,6 +33,7 @@ const routes: Routes = [
     ContactComponent,
     CoursesComponent,
     HomeComponent,
+    NotFoundComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
