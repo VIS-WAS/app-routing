bbd2621881be08a5ccf173ecd887db8ed7a1cd06
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-30 19:15:56 +0530

    navigating around and routerLink example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 //DEFINE ROUTE
 const routes: Routes = [
-  // { path: '', component: HomeComponent },
-  { path: '', redirectTo: 'Home', pathMatch: 'full' }, // we can use line 16 or 17 if empty route to redirect to component
+  { path: '', component: HomeComponent },
   { path: 'Home', component: HomeComponent },
-
+  { path: 'About', component: AboutComponent },
   { path: 'Contact', component: ContactComponent },
   { path: 'Courses', component: CoursesComponent },
   { path: '**', component: NotFoundComponent }, //it should be defined at the end
-  { path: 'About', component: AboutComponent }, // this will also falls under ** so directed to notfound component
 ];
 
 @NgModule({
