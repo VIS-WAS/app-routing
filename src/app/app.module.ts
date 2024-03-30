452f846dca4b0b3432f7e43e3ddf59cf550d0cd7
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-30 18:12:12 +0530

    routing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+//DEFINE ROUTE
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'Home', pathMatch: 'full' }, // we can use line 16 or 17 if empty route to redirect to component
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: 'Courses', component: CoursesComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    CoursesComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
