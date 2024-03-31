f32d064e3e4deeaa55a043ec1146278f46730771
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-31 16:40:47 +0530

    using observable to retrieve route parameter

--- src/app/courses/course-detail/course-detail.component.ts
+++ src/app/courses/course-detail/course-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from 'src/app/Models/course';
 import { CourseService } from 'src/app/Services/course.service';
@@ -8,18 +8,37 @@ import { CourseService } from 'src/app/Services/course.service';
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.css'],
 })
-export class CourseDetailComponent implements OnInit {
+export class CourseDetailComponent implements OnInit, OnDestroy {
   courseService: CourseService = inject(CourseService);
   activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  paramMapObs;
 
   selectedCourse: Course;
   courseID: number;
 
   ngOnInit() {
     // this.courseID = this.activeRoute.snapshot.params['id'];   // old way of getting value of route parameter value
-    this.courseID = +this.activeRoute.snapshot.paramMap.get('id');
-    this.selectedCourse = this.courseService.courses.find(
-      (course) => course.id === this.courseID
-    );
+    //this.courseID = +this.activeRoute.snapshot.paramMap.get('id'); // this can be replace by below. bcz it will not notified when changes on URL happend
+
+    // this.activeRoute.params.subscribe((data) => {
+    //   this.courseID = +data['id'];
+
+    //   this.selectedCourse = this.courseService.courses.find(
+    //     (course) => course.id === this.courseID
+    //   );
+    // });
+
+    this.paramMapObs = this.activeRoute.paramMap.subscribe((data) => {
+      this.courseID = +data.get('id');
+
+      this.selectedCourse = this.courseService.courses.find(
+        (course) => course.id === this.courseID
+      );
+    });
+  }
+
+  ngOnDestroy() {
+    this.paramMapObs.unsubscribe();
   }
 }
