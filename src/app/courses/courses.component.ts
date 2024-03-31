d2cbfb952bdcd42b5840c4269092adb79010ee76
VIS-WAS <vishwas.ms8830@gmail.com>
2024-03-31 12:37:53 +0530

    navigating b/w routes programatically

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Course } from '../Models/course';
+import { CourseService } from '../Services/course.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent {
-
+  courseService = inject(CourseService);
+  Allcourses: Course[] = this.courseService.courses;
 }
--- src/app/home/popular/popular.component.ts
+++ src/app/home/popular/popular.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from 'src/app/Models/course';
 import { CourseService } from 'src/app/Services/course.service';
 
@@ -11,9 +12,18 @@ export class PopularComponent {
   courseService = inject(CourseService);
   popularCourses: Course[] = [];
 
+  router: Router = inject(Router);
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
   ngOnInit() {
     this.popularCourses = this.courseService.courses.filter(
       (c) => c.rating >= 4.5
     );
+  }
+
+  navigateToCouses() {
+    // this.router.navigate(['Courses']);  // this is also written as below
+    this.router.navigateByUrl('Courses'); //both are by default uses absolute path
+    //this.router.navigate(['Courses'], { relativeTo: this.activeRoute });  // by this you can have relative path
   }
 }
