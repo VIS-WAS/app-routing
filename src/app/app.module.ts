86f5f3824a8d6d5b5ca7b8dfef62843f006983ca
VIS-WAS <vishwas.ms8830@gmail.com>
2024-04-01 20:09:03 +0530

    creating route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,28 +9,14 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes } from '@angular/router';
+
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BannerComponent } from './home/banner/banner.component';
 import { PopularComponent } from './home/popular/popular.component';
 import { CourseService } from './Services/course.service';
 import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
 import { ContactUsComponent } from './home/contact-us/contact-us.component';
-
-//DEFINE ROUTE
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'Home', component: HomeComponent },
-  { path: 'About', component: AboutComponent },
-  { path: 'Contact', component: ContactComponent },
-  { path: 'Courses', component: CoursesComponent },
-  // { path: 'Courses/course/:id', component: CourseDetailComponent },
-  {
-    path: 'Courses',
-    children: [{ path: 'course/:id', component: CourseDetailComponent }],
-  },
-  { path: '**', component: NotFoundComponent }, //it should be defined at the end
-];
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
@@ -47,7 +33,7 @@ const routes: Routes = [
     CourseDetailComponent,
     ContactUsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, AppRoutingModule, RoutingModule],
   providers: [CourseService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactComponent } from './contact/contact.component';
+import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: 'Courses', component: CoursesComponent },
+
+  {
+    path: 'Courses',
+    children: [{ path: 'course/:id', component: CourseDetailComponent }],
+  },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class RoutingModule {}
